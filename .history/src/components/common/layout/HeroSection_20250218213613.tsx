188a298ad6da59d17aff5217a37f49d836a30cdd
badhon252 <khalidhossainbadhon@gmail.com>
2025-02-18 21:52:56 +0600

    feat: nav pages route created

---
+++ .history/src/components/common/layout/HeroSection_20250218213613.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <div
+      className="h-[1426px] w-screen bg-contain objec  bg-no-repeat"
+      style={{ backgroundImage: "url('/assets/img/hero_img.png')" }}
+    >
+      HeroSection
+    </div>
+  );
+}
