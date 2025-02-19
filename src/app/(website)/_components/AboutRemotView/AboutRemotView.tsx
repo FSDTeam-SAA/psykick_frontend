2749ebb6e982af6c5e6190e0fc996feb0044d385
ILhankhondaker <ilhankhondaker@gmail.com>
2025-02-19 14:52:37 +0600

    add contact form

--- src/app/(website)/_components/AboutRemotView/AboutRemotView.tsx
+++ src/app/(website)/_components/AboutRemotView/AboutRemotView.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Image from "next/image"
 import { ArrowRight } from "lucide-react"
-import { Button } from "@/components/ui/button"
 import { useEffect, useState } from "react";
 import Link from "next/link";
 
