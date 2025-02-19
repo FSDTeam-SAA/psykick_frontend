6f2c41b632dca68ff8bcf61d93a4cfaeb890a39a
badhon252 <khalidhossainbadhon@gmail.com>
2025-02-19 20:06:58 +0600

    feat: herosection

---
+++ .history/src/components/common/layout/HeroSection_20250219154453.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function HeroSection() {
+  return (
+    <div
+      className="h-[1920px] w-screen bg-cover bg-no-repeat flex  justify-center "
+      style={{ backgroundImage: "url('/assets/img/hero_img.png')" }}
+    >
+      <div className="container">
+        <h1 className="title pt-20">Discover the unseen...</h1>
+        <div>
+          <article className="w-[400px]">
+            Beyond the ordinary lies a realm of hidden knowledge, waiting to be
+            explored. Psykick Club is more than just a gathering—it&apos;s a
+            sanctuary for seekers of the unknown, a place where intuition is
+            sharpened, and perception is tested. Engage in thrilling remote
+            viewing challenges, uncover the secrets of the mind, and connect
+            with a community that dares to see beyond.
+          </article>
+          <article>
+            "The intuitive mind is a sacred gift and the rational mind is a
+            faithful servant. We have created a society that honors the servant
+            and has forgotten the gift." — Carl Jung
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+}
