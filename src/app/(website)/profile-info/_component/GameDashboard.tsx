e236273ac287005a81c482ceb6116f52f9b0ecaf
ILhankhondaker <ilhankhondaker@gmail.com>
2025-04-30 11:32:56 +0600

    add dynamic upper level

--- src/app/(website)/profile-info/_component/GameDashboard.tsx
+++ src/app/(website)/profile-info/_component/GameDashboard.tsx
@@ -5,6 +5,7 @@ import { Card } from "@/components/ui/card";
 import { TemperatureBar } from "./temperature-bar";
 import { LevelIndicator } from "./level-indicator";
 import { WaterBar } from "./water-bar";
+import { useAuth } from "@/hooks/useAuth";
 
 type GameDashboardProps = {
   totalScore: number; // or `number | undefined` if profileData.totalPoints might be undefined
@@ -14,6 +15,36 @@ export default function GameDashboard({ totalScore }: GameDashboardProps) {
 
   console.log(setTemperature);
   console.log("tota",totalScore);
+
+  const {user}  = useAuth()
+  console.log("userxxxx",user?.tierRank);
+  const tierRanks = [
+    "ASCENDING MASTER",
+    "MASTER REMOTE VIEWER",
+    "ORACLE",
+    "SEER",
+    "ADEPT",
+    "VISIONARY",
+    "EXPLORER",
+    "APPRENTICE",
+    "INITIATE",
+    "NOVICE SEEKER",
+  ];
+  
+  let upperRank1 = null;
+  let upperRank2 = null;
+  
+  if (user?.tierRank) {
+    const index = tierRanks.indexOf(user.tierRank);
+    if (index > 0) {
+      upperRank1 = tierRanks[index - 1];
+    }
+    if (index > 1) {
+      upperRank2 = tierRanks[index - 2];
+    }
+  }
+        {/* {upperRank2 && <p>Second highest above: {upperRank2}</p>}
+            {upperRank1 && <p>Highest above: {upperRank1}</p>} */}
 
   return (
     <div className="w-full flex items-center justify-center">
@@ -26,15 +57,16 @@ export default function GameDashboard({ totalScore }: GameDashboardProps) {
           <div className="flex flex-col items-center justify-center h-80 py-4 ">
             <div className="w-full border-dashed border-b-2 border-white py-8">
               <LevelIndicator
-                title="APPRENTICE"
+                title={upperRank2 || ""}
                 bgColor="bg-yellow-400"
                 imageSrc="/assets/profile/apprentice.png"
               />
             </div>
 
             <div className="w-full border-dashed border-b-2 border-white py-8">
+      
               <LevelIndicator
-                title="INITIATE"
+                title= {upperRank1 || ""}
                 bgColor="bg-orange-400"
                 imageSrc="/assets/profile/initiate.png"
               />
@@ -42,7 +74,7 @@ export default function GameDashboard({ totalScore }: GameDashboardProps) {
 
             <div className="pt-8">
               <LevelIndicator
-                title="NOVICE SEEKER"
+                title={user?.tierRank || ""}
                 bgColor="bg-red-500"
                 imageSrc="/assets/profile/novice.png"
               />
